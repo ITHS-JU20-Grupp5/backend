17e73731d9d5aec466e55517eff8ad350874bc0c
Tobias <twadseth@icloud.com>
2021-05-06 14:41:11 +0200

    Added CRUD for users

--- database.js
+++ database.js
@@ -2,9 +2,9 @@ const sqlite = require('sqlite3');
 
 const DBFILE = 'general-knowledge.db';
 
-export const db = new sqlite.Database(DBFILE, (err) => {
+module.exports = new sqlite.Database(DBFILE, (err) => {
     if (err) {
        console.error('Database Error', err.message);
        throw err;
     }
-});
+});;
--- index.js
+++ index.js
@@ -1,10 +1,95 @@
 const express = require('express');
 require('dotenv').config();
+const bodyParser = require('body-parser');
+
+const db = require('./database');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.json({ msg: 'Hello World!' });
+});
+
+app.get('/users', (req, res) => {
+    db.all('select * from users', [], (err, rows) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            ok: true,
+            users: rows
+        });
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+    db.all('select * from users where Id = ?', req.params.id, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            ok: true,
+            user: row
+        });
+    });
+});
+
+/*
+ * Use with following body
+ * {
+ *  "username": "",
+ *  "name": "",
+ *  "email": "",
+ *  "password": "",
+ * }
+ */
+app.post('/users', (req, res) => {
+    let user = [req.body.username, req.body.name, req.body.email, req.body.password];
+    db.run('insert into users (Username, Name, Email, Password) values (?, ?, ?, ?)', user, function (err) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            ok: true,
+            id: this.lastID
+        });
+    });
+});
+
+app.delete('/users/:id', (req, res) => {
+    db.run('delete from users where Id = ?', req.params.id, function (err) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            ok: (this.changes > 0 ? true : false)
+        });
+    });
+});
+
+/*
+ * Use with following body
+ * {
+ *  "password": "",
+ * }
+ */
+app.patch('/users/:id', (req, res) => {
+    db.run('update users set password = ? where Id = ?', [req.body.password, req.params.id], function (err) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            ok: (this.changes > 0 ? true : false)
+        })
+    })
 });
 
 const port = process.env.PORT || 3000;
