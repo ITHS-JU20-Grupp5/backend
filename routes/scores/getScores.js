f723510b36764d3426e567f886691283d655efa0
Tobias <twadseth@icloud.com>
2021-05-11 15:40:15 +0200

    Added return statement for 0 rows

--- routes/scores/getScores.js
+++ routes/scores/getScores.js
@@ -6,15 +6,23 @@ module.exports = function (app) {
         let userId = req.user.Id;
         let scores = [];
         db.all('select * from user_scores where UserId = ?', [userId], function (err, rows) {
-            if(err) {
+            if (err) {
                 res.json({
                     ok: false,
                     error: err.message
                 });
             }
             let index = 0;
+            if (rows.length === 0) {
+                res.json({
+                    ok: true,
+                    scores: []
+                });
+                return;
+            }
             rows.forEach(row => {
                 db.get('select users.Id, Score from scores inner join users where scores.Id = ? and users.Id = ?', [row.ScoreId, row.UserId], function (err, row) {
+                    console.log(row);
                     scores = [row, ...scores];
                     index++;
                     if (index === rows.length) {
