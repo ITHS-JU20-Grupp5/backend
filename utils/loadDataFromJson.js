02ddd669ebd25f486604ac63ee61dc5431666b47
Tobias <twadseth@icloud.com>
2021-05-13 14:37:19 +0200

    Added option for question amount to getQuestions and fallback of min

---
+++ utils/loadDataFromJson.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+
+const http = axios.create({ baseURL: 'http://localhost:3000' });
+
+let jsonFilePath = process.argv.splice(2)[0];
+
+const data = require(`../${jsonFilePath}`);
+
+data.forEach(async (obj) => {
+  const { category, questions } = obj;
+  const categoryPost = await http.post('/categories', { category });
+  questions.forEach(async (questionObj) => {
+    const { question, answers } = questionObj;
+    const questionPost = await http.post(`/categories/${categoryPost.data.id}/questions`, {
+      question,
+    });
+    answers.forEach((answerObj) => {
+      http.post(`/questions/${questionPost.data.id}/answers`, answerObj);
+    });
+  });
+});
