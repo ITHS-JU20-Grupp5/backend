dce51437f257822504e962a49a098ac28130a9d8
Tobias <twadseth@icloud.com>
2021-05-17 13:56:35 +0200

    Refactored loadDataFromJson

--- utils/loadDataFromJson.js
+++ utils/loadDataFromJson.js
@@ -6,20 +6,34 @@ const jsonFilePath = process.argv.splice(2)[0];
 
 const data = require(`../${jsonFilePath}`);
 
-data.users.forEach(async (user) => {
+data.users.forEach((user) => {
   http.post('/users', user);
 });
 
-data.quiz.forEach(async (obj) => {
-  const { category, questions } = obj;
-  const categoryPost = await http.post('/categories', { category });
-  questions.forEach(async (questionObj) => {
-    const { question, answers } = questionObj;
-    const questionPost = await http.post(`/categories/${categoryPost.data.id}/questions`, {
-      question,
+data.quiz.forEach((quizObj) => {
+  const { category, questions } = quizObj;
+  http
+    .post('/categories', { category })
+    .then((categoryPost) => {
+      questions.forEach((questionObj) => {
+        const { question, answers } = questionObj;
+        http
+          .post(`/categories/${categoryPost.data.id}/questions`, { question })
+          .then((questionPost) => {
+            answers.forEach((answerObj) => {
+              http.post(`/questions/${questionPost.data.id}/answers`, answerObj);
+            });
+          })
+          .catch((questionError) => {
+            if (questionError) {
+              console.error(questionError.message);
+            }
+          });
+      });
+    })
+    .catch((categoryError) => {
+      if (categoryError) {
+        console.error(categoryError.message);
+      }
     });
-    answers.forEach((answerObj) => {
-      http.post(`/questions/${questionPost.data.id}/answers`, answerObj);
-    });
-  });
 });
