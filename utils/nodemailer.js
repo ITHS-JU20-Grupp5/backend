fef01ae7e49b0b5e66791e577c52ff7fe5f59b79
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2021-05-26 09:39:43 +0200

    Email is ALIVE and functional

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -1,42 +1,49 @@
-//Our email:
-//general-knowledge-quiz@outlook.com
-//ITHSJU20-THE-FIFTH-GROUP
-//smtp-mail.outlook.com
-//port 587 || port 25
-//SMTP TLS/SSL Encryption Required? You betcha.
-
-//Server name: smtp.office365.com
-//Encryption method: STARTTLS
-
-//This is a complete example to send an email with plain text and HTML body
-"use strict";
 const nodemailer = require('nodemailer');
-const nodeoutlook = require('nodejs-nodemailer-outlook')
-const bodyParser = require('body-parser');
+require('dotenv').config();
 
-    // create reusable transporter object using the default SMTP transport
-    const transporter = nodemailer.createTransport({
-        host: "smtp.office365.com",
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: 'general-knowledge-quiz@outlook.com', // generated ethereal user
-            pass: 'ITHSJU20-THE-FIFTH-GROUP', // generated ethereal password
-        },
-    });
+//variable for obtaining user email from register.js THIS DOES NOT WORK
+//const { userEmail } = require.main.require('./auth/register');
 
-    // send mail with defined transport object
+//secret variables for sending email including login credentials
+const email = process.env.EMAIL;
+const password = process.env.EMAILPASS;
+const port = parseInt(process.env.EMAILPORT);
+const host = process.env.EMAILHOST;
+const group5 = 'thomas.lloyd.jones@iths.se, tobias.wadseth@iths.se, karl.albin.jartun@iths.se, gustaf.linnarsson@iths.se, kennie.svensson@iths.se, yonas.romell@iths.se'
+const backEndCrew = 'thomas.lloyd.jones@iths.se, tobias.wadseth@iths.se, karl.albin.jartun@iths.se, gustaf.linnarsson@iths.se'
+const me = 'thomas.lloyd.jones@iths.se'
+
+// create reusable transporter object using the default SMTP transport
+const transport = nodemailer.createTransport({
+    host: host,
+    port: port,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: email,
+        pass: password
+    },
+});
+
+//module.exports = function () {
+    //Code to send email using transport object defined above
     const options = {
-        from: '"General Chaos" <general-knowledge-quiz@outlook.com>', // sender address
-        to: 'thomas.lloyd.jones@iths.se, gustaf.linnarsson@iths.se, karl.albin.jartun@iths.se, tobias.wadseth@iths.se', // list of receivers
-        subject: 'The General has a message for you...', // Subject line
-        text: 'GET DOWN AND GIVE ME 50... questions \nWelcome to General Knowledge. Please verify your account by following this link:', // plain text body
-        //html: '<b>Welcome to General Knowledge. Please verify your account by following this link:</b>', // html body
+        from: '"General Chaos" <general-knowledge-quiz@outlook.com>',
+        to: me,// userEmail, //register.req.body.email,
+        subject: 'The General needs YOU...',
+        text: '...TO GET DOWN AND GIVE ME 50... answers to the following questions of the day. \nWelcome to General Knowledge. Please verify your account by following this link:',
+        attachments: [{
+            filename: 'General.jpg',
+            path: './assets/General.jpg',
+            cid: 'General'
+        }]
+        //html: '<b>link to validation page</b>',
     };
 
-    transporter.sendMail(options, function(error, info){
-        if(error){
+    transport.sendMail(options, function (error, info) {
+        if (error) {
             return console.log(error);
         }
         console.log('Success: ' + info.response);
     })
+//};
+console.log('Email: ' + email + '\nPassword: ' + password + '\nHost: ' + host + '\nPort: ' + port);
