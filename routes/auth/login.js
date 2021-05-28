524278b3acc282e6d895ea5c136f46c72709ace5
Tobias Wadseth <twadseth@icloud.com>
2021-05-28 15:04:26 +0200

    Added verification restriction to login
    * Bump dotenv, eslint-plugin-import, and eslint
    
    * Add or update the Azure App Service build and deployment workflow config
    
    * Added sequelize with PostgreSQL and emails
    
    * Implemented PostgreSQL with Sequelize
    
    * Started adding models and associations with sequelize
    
    * Updated to PostGre and sequelize
    
    * Started rewriting routes using sequelize
    
    * Updated routes with sequelize and added func to models
    
    * amended updateUserCredentials for Postgres
    
    * Updated createScore and started working on getQuiz
    
    * Finished up getQuiz with PostgreSQL and sequelize
    
    Co-authored-by: Gurra <g.linnar@gmail.com>
    Co-authored-by: Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
    
    * Added ID:s to quiz object
    
    * Added isAdmin route
    
    * nodemailer (#50)
    
    * Added verification model, controller, .js and associations, also updated user controller etc etc
    
    * nodemailer first try (#49)
    
    * linked up to email account in nodemailer.js, and capable of sending emails via node nodemailer.js
    
    * Email is ALIVE and functional
    
    Co-authored-by: Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
    
    * Added mail files and updated models and controllers
    
    * Updated score
    
    Co-authored-by: Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
    
    * Fixed url error in register
    
    * Added dev script for windows
    
    Co-authored-by: Gurra <g.linnar@gmail.com>
    Co-authored-by: Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
    
    * Bump dotenv from 9.0.2 to 10.0.0
    
    Bumps [dotenv](https://github.com/motdotla/dotenv) from 9.0.2 to 10.0.0.
    - [Release notes](https://github.com/motdotla/dotenv/releases)
    - [Changelog](https://github.com/motdotla/dotenv/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/motdotla/dotenv/compare/v9.0.2...v10.0.0)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump eslint-plugin-import from 2.23.2 to 2.23.3
    
    Bumps [eslint-plugin-import](https://github.com/benmosher/eslint-plugin-import) from 2.23.2 to 2.23.3.
    - [Release notes](https://github.com/benmosher/eslint-plugin-import/releases)
    - [Changelog](https://github.com/benmosher/eslint-plugin-import/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/benmosher/eslint-plugin-import/compare/v2.23.2...v2.23.3)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump eslint from 7.26.0 to 7.27.0
    
    Bumps [eslint](https://github.com/eslint/eslint) from 7.26.0 to 7.27.0.
    - [Release notes](https://github.com/eslint/eslint/releases)
    - [Changelog](https://github.com/eslint/eslint/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/eslint/eslint/compare/v7.26.0...v7.27.0)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: Gurra <g.linnar@gmail.com>
    Co-authored-by: Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Fixed broken azure links
    
    * Spell checked questions
    
    * Spellcheck
    
    * Updated question
    
    * Added english questions
    
    * Added cors origins
    
    * updated user verification redirection to frontend
    
    * Added preflightContinue
    
    * Removed preflight, added nonssl origin
    
    * Added verification restriction to login
    
    Co-authored-by: Gurra <g.linnar@gmail.com>
    Co-authored-by: Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Kabbej <kabbe.jartun@gmail.com>

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -2,38 +2,47 @@ const { password } = require.main.require('./utils/utilities');
 const jwt = require('jsonwebtoken');
 
 const UserController = require.main.require('./controllers/user.controller');
+const RoleController = require.main.require('./controllers/role.controller');
 
 module.exports = (app) => {
-  app.post('/auth/login', async (req, res) => {
-    UserController.findOne({ username: req.body.username }).then(async (user) => {
+  app.post('/auth/login', (req, res) => {
+    UserController.findOne({ username: req.body.username }).then((user) => {
       if (!user) {
         res.status(400).json({
           error: 'User does not exist',
         });
         return;
       }
-      const verified = await password.verify(req.body.password, user.password);
-      if (!verified) {
-        res.status(400).json({
-          error: 'Invalid password',
+      RoleController.findOne({ role: 'User' }).then(async (role) => {
+        if (!user.hasRole(role)) {
+          res.status(401).json({
+            message: 'Please verify your account',
+          });
+          return;
+        }
+        const verified = await password.verify(req.body.password, user.password);
+        if (!verified) {
+          res.status(400).json({
+            error: 'Invalid password',
+          });
+          return;
+        }
+        user = JSON.parse(JSON.stringify(user, null, 4));
+        const accessToken = jwt.sign(user, process.env.TOKENSECRET, {
+          expiresIn: '30d',
         });
-        return;
-      }
-      user = JSON.parse(JSON.stringify(user, null, 4));
-      const accessToken = jwt.sign(user, process.env.TOKENSECRET, {
-        expiresIn: '30d',
+        const newUser = {
+          username: user.username,
+          name: user.name,
+          email: user.email,
+          password: user.password,
+          roles: user.roles,
+          createdAt: user.createdAt,
+          updatedAt: user.updatedAt,
+          accessToken,
+        };
+        res.json(newUser);
       });
-      const newUser = {
-        username: user.username,
-        name: user.name,
-        email: user.email,
-        password: user.password,
-        roles: user.roles,
-        createdAt: user.createdAt,
-        updatedAt: user.updatedAt,
-        accessToken,
-      };
-      res.json(newUser);
     });
   });
 };
