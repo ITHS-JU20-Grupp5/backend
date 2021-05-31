bf063c4410fe4d66c453c64d18277e5ba29a53a2
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2021-05-31 09:56:45 +0200

    commented out spam functions in index and nodeMailer as we reached the limit in outgoing emails (and wish to avoid legal complications... ;-) )

--- index.js
+++ index.js
@@ -54,22 +54,24 @@ db.sequelize.sync(options).then(async () => {
 
   recursiveRoutes('routes');
 
-  cron.schedule(
-    '0 9 * * *',
-    () => {
-      UserController.findAll().then((users) => {
-        users.forEach((user) => {
-          if (user.spam) {
-            sendSpam(user.email);
-          }
-        });
-      });
-    },
-    {
-      scheduled: true,
-      timezone: 'Europe/Stockholm',
-    }
-  );
+  //Newsletter function below removed from production due to email numerical constraints (but it works ;-) )
+
+  // cron.schedule(
+  //   '0 9 * * *',
+  //   () => {
+  //     UserController.findAll().then((users) => {
+  //       users.forEach((user) => {
+  //         if (user.spam) {
+  //           sendSpam(user.email);
+  //         }
+  //       });
+  //     });
+  //   },
+  //   {
+  //     scheduled: true,
+  //     timezone: 'Europe/Stockholm',
+  //   }
+  // );
 
   const port = process.env.PORT || 3000;
   const url =
